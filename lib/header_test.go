b50e34070f3e1bf0722c99d9c4a3975509ae4fee
Ciro S. Costa <ciro.costa@liferay.com>
2017-12-29 12:17:32 -0200

    Improves header testing

--- lib/header_test.go
+++ lib/header_test.go
@@ -14,28 +14,65 @@ func TestHeaderMarshallingAndUnmarshalling(t *testing.T) {
 		shouldError bool
 	}{
 		{
-			desc: "ID gets properly splitted into the first two bytes",
+			desc:   "0-ed case",
+			header: &Header{},
+		},
+		{
+			desc: "case all set",
 			header: &Header{
-				ID: 2,
+				ID:      2,
+				QR:      1,
+				Opcode:  OpcodeQuery,
+				AA:      1,
+				TC:      1,
+				RD:      1,
+				RA:      1,
+				Z:       1,
+				RCODE:   1,
+				QDCOUNT: 1,
+				ANCOUNT: 1,
+				NSCOUNT: 1,
+				ARCOUNT: 1,
+			},
+		},
+		{
+			desc: "some set",
+			header: &Header{
+				ID:      333,
+				QR:      1,
+				Opcode:  OpcodeQuery,
+				AA:      1,
+				TC:      0,
+				RD:      1,
+				RA:      0,
+				Z:       1,
+				RCODE:   1,
+				QDCOUNT: 0,
+				ANCOUNT: 1,
+				NSCOUNT: 0,
+				ARCOUNT: 2,
 			},
 		},
 	}
 
 	var (
-		msg []byte
-		err error
+		msg                []byte
+		err                error
+		unarmshalledHeader *Header
 	)
 
 	for _, tc := range testCases {
 		t.Run(tc.desc, func(t *testing.T) {
 			msg, err = tc.header.Marshal()
-			if tc.shouldError {
-				assert.Error(t, err)
-				return
-			}
-
 			require.NoError(t, err)
 			assert.Equal(t, 12, len(msg))
+
+			unarmshalledHeader = new(Header)
+			err = UnmarshalHeader(msg, unarmshalledHeader)
+			require.NoError(t, err)
+
+			assert.Equal(t, tc.header.ID, unarmshalledHeader.ID)
+			assert.Equal(t, tc.header.QR, unarmshalledHeader.QR)
 		})
 	}
 }
